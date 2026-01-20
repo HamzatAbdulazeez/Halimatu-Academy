074e0c930451ae5a92398d133224a261963b834c
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2026-01-20 19:40:43 +0100

    Update

---
+++ src/components/ui/NotFoundPage.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 px-6">
+      {/* Logo */}
+      <Link to="/"> <div className="w-32 h-8 bg-gray-300 rounded flex items-center justify-center">
+        <span className="text-black text-sm">LOGO</span>
+      </div></Link>
+
+      {/* 404 Text */}
+      <h1 className="text-9xl font-extrabold text-[#004AAD]">404</h1>
+      <h2 className="text-3xl font-semibold mt-4 text-gray-800">Page Not Found</h2>
+      <p className="text-gray-600 mt-2 text-lg">
+        Sorry, the page you're looking for doesn't exist or has been moved.
+      </p>
+
+      {/* Animation */}
+      <div className="mt-6">
+        <img
+          src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1743157631/AoStyle/DALL_E_2025-03-28_11.26.35_-_A_modern_and_clean_404_error_page_illustration_featuring_a_broken_road_sign_with_404_written_on_it_placed_in_a_minimalistic_landscape._A_confused_c_wbubju.webp"
+          alt="Not Found Illustration"
+          className="w-72"
+        />
+      </div>
+
+      {/* Button to Go Home */}
+      <Link
+        to="/"
+        className="mt-6 px-6 py-3 bg-gradient text-white text-lg rounded-lg  hover:bg-purple-700 transition"
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
--- src/routes/landingRoute.jsx
+++ src/routes/landingRoute.jsx
@@ -1,7 +1,7 @@
 import LandingLayout from "../layouts/landing";
 import LandingHomepage from "../modules/Home";
 import AboutUs from "../modules/Home/About";
-
+import NotFoundPage from "../components/ui/NotFoundPage";
 
 export const landingRooutes = [
   {
@@ -15,6 +15,10 @@ export const landingRooutes = [
       {
         path: 'about',
         element: <AboutUs />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
       },
     ],
   },
